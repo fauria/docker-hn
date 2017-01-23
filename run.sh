91f0d5da850696deb0fd458caf2f1dd49fe649d0
Fer Uría <fauria@gmail.com>
2017-01-23 12:58:28 +0100

    Removed sed statement after @akkartik commit 5460fad

--- run.sh
+++ run.sh
@@ -7,16 +7,13 @@ mkdir -p www/news/story
 mkdir -p www/news/vote
 mkdir -p www/tmp
 
+# Set site attributes and color:
 sed -ie "s/http:\/\/news.yourdomain.com\//$(echo $SITE_URL|sed -e 's/[\/&]/\\&/g')/g" lib/news.arc
 sed -ie "s/http:\/\/www.yourdomain.com/$(echo $PARENT_URL|sed -e 's/[\/&]/\\&/g')/g" lib/news.arc
 sed -ie "s/My Forum/$(echo $SITE_NAME|sed -e 's/[\/&]/\\&/g')/g" lib/news.arc
 sed -ie "s/What this site is about./$(echo $SITE_DESCRIPTION|sed -e 's/[\/&]/\\&/g')/g" lib/news.arc
 sed -ie "s/favicon-url\*  \"\"/favicon-url\*  \"$(echo $FAVICON_URL|sed -e 's/[\/&]/\\&/g')\"/g" lib/news.arc
-
 sed -ie "s/color 180 180 180/color $((16#${RGB_COLOR:0:2})) $((16#${RGB_COLOR:2:2})) $((16#${RGB_COLOR:4:2}))/g" lib/news.arc
-
-# Place tmp irectory inside exported volume to avoid system error: Invalid cross-device link; errno=18
-sed -ie 's/tmpdir\* \"tmp\"/tmpdir\* \"www\/tmp\"/g' arc.arc
 
 # If defined and not pressent, add Google Analytics tracking code:
 ga_line=$(grep 'pr ga' lib/news.arc|wc -l)
